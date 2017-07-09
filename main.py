42160d2432229c86df0e5943565e55803edbda65
Jonathan Sanchez <jonsanchezu@gmail.com>
2017-07-08 22:55:46 -0700

    added Blog class with properties

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:blogs@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
 
 class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
