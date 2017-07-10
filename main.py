014ae2d2322ad2c537f8d443dc9f0349521859e5
Jonathan Sanchez <jonsanchezu@gmail.com>
2017-07-10 16:58:54 -0700

    fixed error messages

--- main.py
+++ main.py
@@ -16,38 +16,34 @@ class Blog(db.Model):
         self.title = title
         self.body = body
 
-@app.route('/blog', methods=['POST', 'GET'])
+@app.route('/blog')
 def index():
-
-    if request.method == 'POST':
-        blog_title = request.form['title']
-        blog_body = request.form['body']
-        new_blog = Blog(blog_title, blog_body)
-        db.session.add(new_blog)
-        db.session.commit()
-
     blogs = Blog.query.all()
     return render_template('blog.html', title="Build A Blog", blogs = blogs)
 
-@app.route('/newpost', methods=['POST', 'GET'])
+@app.route('/newpost')
 def new_post():
-    if request.method == 'GET':
-        return render_template('newpost.html', title="Add Blog Entry")
-    else:
-        blog_title = request.form['title']
-        blog_body = request.form['body']
-        title_error = ''
-        body_error = ''
+    return render_template('newpost.html', title="Add Blog Entry")
 
-        if blog_title == "":
-            title_error = "Please fill in the title"
-        if blog_body == "":
-            body_error = "Please fill in the body"
-        
-        if not title_error and not body_error:
-            return redirect('/blog', title=blog_title)    
-        else:
-            return render_template('newpost.html', title="Add Blog Entry", title_error = title_error, body_error = body_error)
+@app.route('/newpost', methods=['POST'])
+def verify_post():
+    blog_title = request.form['title']
+    blog_body = request.form['body']
+    title_error = ''
+    body_error = ''
+
+    if blog_title == "":
+        title_error = "Please fill in the title"
+    if blog_body == "":
+        body_error = "Please fill in the body"
+    
+    if not title_error and not body_error:
+        new_blog = Blog(blog_title, blog_body)
+        db.session.add(new_blog)
+        db.session.commit()
+        return redirect('/blog')
+    else:
+        return render_template('newpost.html', title="Add Blog Entry", blog_title = blog_title, blog_body = blog_body, title_error = title_error, body_error = body_error)
 
 if __name__ == '__main__':
     app.run()
